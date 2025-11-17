172ed62e1526c1a7addec5e343f66d5f7b6278c0
skywh <skywhymuted@gmail.com>
2025-11-17 16:46:41 +0800

    Passed 2 ✅

--- python_project_scripts/scripts/gendiff.py
+++ python_project_scripts/scripts/gendiff.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 
 def main():
     parser = argparse.ArgumentParser(
@@ -11,6 +13,15 @@ def main():
     parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
+
+    file1_path = args.first_file
+    file2_path = args.second_file
+
+    file1 = json.load(open(file1_path))
+    file2 = json.load(open(file2_path))
+
+    print(f'file1: {file1}')
+    print(f'file2: {file2}')
 
 if __name__ == "__main__":
     main()
